5f2a4b50318fc1c460124e23cc61e0671ce32550
Omadbek2009 <toxtasinovomadbek054@gmail.com>
2022-11-02 15:42:58 +0500

    Tahrirlandi

--- user.js
+++ user.js
@@ -1,59 +1,100 @@
 const users = [
     {
-        ism: "Omadbek",
-        fam: "Toxtasinov",
+        id: 1,
+        name: "Omadbek",
+        surname: "Toxtasinov",
         phone: "+998902205136",
-        email: "toxtasinovomadbek054@gmail.com",
-        image: "https://www.google.com/imgres?imgurl=https%3A%2F%2Fcdn.forbes.ru%2Ffiles%2Fprofile%2Fsteve_jobs.jpg__1584098747__37025.jpg&imgrefurl=https%3A%2F%2Fwww.forbes.ru%2Fprofile%2F394951-stiv-dzhobs-steve-jobs&tbnid=TTWlIDwt0ggHMM&vet=12ahUKEwiM9eHYptH6AhVxwosKHcc4DXsQMyg4egQIARBE..i&docid=JU5zzNIBfrz7PM&w=900&h=566&q=steve%20jobs&ved=2ahUKEwiM9eHYptH6AhVxwosKHcc4DXsQMyg4egQIARBE"
+        email: "omadweb@gmail.com",
+        image: "https://images2.alphacoders.com/984/984402.jpg"
     },
     {
-        ism: "Saydullo",
-        fam: "Ergashev",
+        id: 2,
+        name: "Saydullo",
+        surname: "Ergashev",
         phone: "+998911740482",
         email: "ergashev@gmail.com",
-        image: "https://www.google.com/imgres?imgurl=https%3A%2F%2Fcdn.forbes.ru%2Ffiles%2Fprofile%2Fsteve_jobs.jpg__1584098747__37025.jpg&imgrefurl=https%3A%2F%2Fwww.forbes.ru%2Fprofile%2F394951-stiv-dzhobs-steve-jobs&tbnid=TTWlIDwt0ggHMM&vet=12ahUKEwiM9eHYptH6AhVxwosKHcc4DXsQMyg4egQIARBE..i&docid=JU5zzNIBfrz7PM&w=900&h=566&q=steve%20jobs&ved=2ahUKEwiM9eHYptH6AhVxwosKHcc4DXsQMyg4egQIARBE"
+        image: "https://images2.alphacoders.com/984/984402.jpg"
     },
     {
-        ism: "Kamoliddin",
-        fam: "Nazrullayev",
+        id: 3,
+        name: "Kamoliddin",
+        surname: "Nazrullayev",
         phone: "+998954561221",
         email: "kamoldin@gmail.com",
-        image: "https://www.google.com/imgres?imgurl=https%3A%2F%2Fcdn.forbes.ru%2Ffiles%2Fprofile%2Fsteve_jobs.jpg__1584098747__37025.jpg&imgrefurl=https%3A%2F%2Fwww.forbes.ru%2Fprofile%2F394951-stiv-dzhobs-steve-jobs&tbnid=TTWlIDwt0ggHMM&vet=12ahUKEwiM9eHYptH6AhVxwosKHcc4DXsQMyg4egQIARBE..i&docid=JU5zzNIBfrz7PM&w=900&h=566&q=steve%20jobs&ved=2ahUKEwiM9eHYptH6AhVxwosKHcc4DXsQMyg4egQIARBE"
+        image: "https://images2.alphacoders.com/984/984402.jpg"
     },
     {
-        ism: "Hushnudbek",
-        fam: "",
+        id: 4,
+        name: "Hushnudbek",
+        surname: "",
         phone: "+998996787887",
         email: "khushnud@gmail.com",
-        image: "https://www.google.com/imgres?imgurl=https%3A%2F%2Fcdn.forbes.ru%2Ffiles%2Fprofile%2Fsteve_jobs.jpg__1584098747__37025.jpg&imgrefurl=https%3A%2F%2Fwww.forbes.ru%2Fprofile%2F394951-stiv-dzhobs-steve-jobs&tbnid=TTWlIDwt0ggHMM&vet=12ahUKEwiM9eHYptH6AhVxwosKHcc4DXsQMyg4egQIARBE..i&docid=JU5zzNIBfrz7PM&w=900&h=566&q=steve%20jobs&ved=2ahUKEwiM9eHYptH6AhVxwosKHcc4DXsQMyg4egQIARBE"
+        image: "https://images2.alphacoders.com/984/984402.jpg"
     },
     {
-        ism: "Hsanboy",
-        fam: "Jumanov",
+        id: 5,
+        name: "Hasanboy",
+        surname: "Jumanov",
         phone: "+9983334444",
         email: "jumanov@gmail.com",
-        image: "https://www.google.com/imgres?imgurl=https%3A%2F%2Fcdn.forbes.ru%2Ffiles%2Fprofile%2Fsteve_jobs.jpg__1584098747__37025.jpg&imgrefurl=https%3A%2F%2Fwww.forbes.ru%2Fprofile%2F394951-stiv-dzhobs-steve-jobs&tbnid=TTWlIDwt0ggHMM&vet=12ahUKEwiM9eHYptH6AhVxwosKHcc4DXsQMyg4egQIARBE..i&docid=JU5zzNIBfrz7PM&w=900&h=566&q=steve%20jobs&ved=2ahUKEwiM9eHYptH6AhVxwosKHcc4DXsQMyg4egQIARBE"
+        image: "https://images2.alphacoders.com/984/984402.jpg"
     },
     {
-        ism: "Abdurasul",
-        fam: "Haydarov",
+        id: 6,
+        name: "Abdurasul",
+        surname: "Haydarov",
         phone: "+998334001274",
         email: "haydarov@gmail.com",
-        image: "https://www.google.com/imgres?imgurl=https%3A%2F%2Fcdn.forbes.ru%2Ffiles%2Fprofile%2Fsteve_jobs.jpg__1584098747__37025.jpg&imgrefurl=https%3A%2F%2Fwww.forbes.ru%2Fprofile%2F394951-stiv-dzhobs-steve-jobs&tbnid=TTWlIDwt0ggHMM&vet=12ahUKEwiM9eHYptH6AhVxwosKHcc4DXsQMyg4egQIARBE..i&docid=JU5zzNIBfrz7PM&w=900&h=566&q=steve%20jobs&ved=2ahUKEwiM9eHYptH6AhVxwosKHcc4DXsQMyg4egQIARBE"
+        image: "https://images2.alphacoders.com/984/984402.jpg"
     },
     {
-        ism: "Ozodbek",
-        fam: "Turgunboyev",
+        id: 7,
+        name: "Ozodbek",
+        surname: "Turgunboyev",
         phone: "+998234234563",
         email: "turgunboyev@gmail.com",
-        image: "https://www.google.com/imgres?imgurl=https%3A%2F%2Fcdn.forbes.ru%2Ffiles%2Fprofile%2Fsteve_jobs.jpg__1584098747__37025.jpg&imgrefurl=https%3A%2F%2Fwww.forbes.ru%2Fprofile%2F394951-stiv-dzhobs-steve-jobs&tbnid=TTWlIDwt0ggHMM&vet=12ahUKEwiM9eHYptH6AhVxwosKHcc4DXsQMyg4egQIARBE..i&docid=JU5zzNIBfrz7PM&w=900&h=566&q=steve%20jobs&ved=2ahUKEwiM9eHYptH6AhVxwosKHcc4DXsQMyg4egQIARBE"
+        image: "https://images2.alphacoders.com/984/984402.jpg"
     },
     {
-        ism: "Eldorbek",
-        fam: "Qobulov",
+        id: 8,
+        name: "Eldorbek",
+        surname: "Qobulov",
         phone: "+99890210452",
         email: "khabulov@gmail.com",
-        image: "https://www.google.com/imgres?imgurl=https%3A%2F%2Fcdn.forbes.ru%2Ffiles%2Fprofile%2Fsteve_jobs.jpg__1584098747__37025.jpg&imgrefurl=https%3A%2F%2Fwww.forbes.ru%2Fprofile%2F394951-stiv-dzhobs-steve-jobs&tbnid=TTWlIDwt0ggHMM&vet=12ahUKEwiM9eHYptH6AhVxwosKHcc4DXsQMyg4egQIARBE..i&docid=JU5zzNIBfrz7PM&w=900&h=566&q=steve%20jobs&ved=2ahUKEwiM9eHYptH6AhVxwosKHcc4DXsQMyg4egQIARBE"
+        image: "https://images2.alphacoders.com/984/984402.jpg"
+    },
+    {
+        id: 9,
+        name: "Begzodbek",
+        surname: "Turgunov",
+        phone: "+998939333939",
+        email: "ali@gmail.com",
+        image: "https://images2.alphacoders.com/984/984402.jpg"
+    },
+    {
+        id: 10,
+        name: "Iqboljon",
+        surname: "Raimjonov",
+        phone: "+998939334438",
+        email: "raimov@gmail.com",
+        image: "https://images2.alphacoders.com/984/984402.jpg"
+    },
+    {
+        id: 11,
+        name: "Mashxurbek",
+        surname: "Hoshimov",
+        phone: "+998888991281",
+        email: "khoshimov@gmail.com",
+        image: "https://images2.alphacoders.com/984/984402.jpg"
+    },
+    {
+        id: 12,
+        name: "Hamidullo",
+        surname: "Habibullayev",
+        phone: "+998901743550",
+        email: "hanibullayev@gmail.com",
+        image: "https://images2.alphacoders.com/984/984402.jpg"
     }
+
 ]
 module.exports = users
